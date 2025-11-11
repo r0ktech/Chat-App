d9b152a30cb7fda8edcf9dbd1491d528fd49bd3d
Raphael Okeke <raphaelokeke002@gmail.com>
2025-11-11 21:12:18 +0100

    new code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,3 @@
-const { use } = require("react");
-
 const socket = io();
 
 // Elements
@@ -15,7 +13,7 @@ const locationMessageTemplate = document.querySelector(
   "#location-message-template"
 ).innerHTML;
 
-//Options
+// Options
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
@@ -23,6 +21,7 @@ const { username, room } = Qs.parse(location.search, {
 socket.on("message", (message) => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
@@ -32,6 +31,7 @@ socket.on("message", (message) => {
 socket.on("locationMessage", (message) => {
   console.log(message);
   const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
     url: message.url,
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
@@ -83,6 +83,6 @@ $sendLocationButton.addEventListener("click", () => {
 socket.emit("join", { username, room }, (error) => {
   if (error) {
     alert(error);
-    location.href = '/'
+    location.href = "/";
   }
 });
--- src/utils/users.js
+++ src/utils/users.js
@@ -1,33 +1,30 @@
 const users = [];
 
-// addUser, removeUser, getUser, getUserInRoom
-
 const addUser = ({ id, username, room }) => {
-  // Clear the data
+  // Clean the data
   username = username.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
-  //Validate the data
+  // Validate the data
   if (!username || !room) {
     return {
-      error: "Username and room are required",
+      error: "Username and room are required!",
     };
   }
 
-  //Check for existing user
+  // Check for existing user
   const existingUser = users.find((user) => {
     return user.room === room && user.username === username;
   });
 
-  //Validate username
+  // Validate username
   if (existingUser) {
     return {
-      error: "Username is in use !",
+      error: "Username is in use!",
     };
   }
 
-  //Store user
-
+  // Store user
   const user = { id, username, room };
   users.push(user);
   return { user };
